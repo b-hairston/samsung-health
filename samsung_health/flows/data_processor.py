1fb1f41718f331c1464bb1be5df0985f605e8748
Ben Hairston <bph484@gmail.com>
2025-01-20 00:37:03 -0600

    updated README.md

--- samsung_health/flows/data_processor.py
+++ samsung_health/flows/data_processor.py
@@ -31,9 +31,18 @@ async def process_exercises(exercise_df: pl.DataFrame) -> List[Exercise]:
             'max_rpm',
             'custom',
             'title'])
-    validated_exercises_df= validated_exercises_df.select([
-        pl.col(col).fill_nan(pl.lit(0) if dtype in [pl.Int64, pl.Float64] else pl.lit('Unknown'))
-        for col, dtype in zip(validated_exercises_df.columns, validated_exercises_df.dtypes)])
+    float_columns = [col for col, dtype in validated_exercises_df.schema.items() if dtype == pl.Float64]
+    datetime_columns = [col for col, dtype in validated_exercises_df.schema.items() if dtype == pl.Datetime]
+    int_columns = [col for col, dtype in validated_exercises_df.schema.items() if dtype == pl.Int64]
+    string_columns = [col for col, dtype in validated_exercises_df.schema.items() if dtype == pl.Utf8]
+
+    validated_exercises_df = validated_exercises_df.with_columns([
+        *[pl.col(col).fill_nan(0).alias(col) for col in float_columns],
+        *[pl.col(col).fill_null(pl.datetime(1970, 1, 1)).alias(col) for col in datetime_columns],
+        *[pl.col(col).fill_null(0).alias(col) for col in int_columns],  # Replace null with 0 for integers
+        *[pl.col(col).fill_null("").alias(col) for col in string_columns],  # Replace null with an empty string for strings
+])
+    breakpoint()
     # write df to file
     now = datetime.now(timezone.utc)
     timestamp = now.strftime("%Y%m%d_%H%M%S")
